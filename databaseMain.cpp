1038e522e5ddb16a19e42cf4d5035543e57bb0dd
shreyamohan <mohanshreya1@gmail.com>
2024-01-01 14:33:03 -0800

    cleaned

--- databaseMain.cpp
+++ databaseMain.cpp
@@ -5,23 +5,6 @@
 
 int main()
 {
-  // Database db;
-  // string filename = "/home/csmajs/smoha095/final-project-smoha095-ashah174-ayama039-skoth011/ movies.tsv";
-  // db.loadFromTSV(filename);
-  // vector<Movie> ageMovies = db.filter("Drama", 13, "Kevin Zaideman", "Christopher Nolan");
-  // for (int i = 0; i < ageMovies.size(); i++)
-  // {
-  //   ageMovies[i].display();
-  // }
-  // struct preferences preference;
-  // preference.actor = "Timothée Chalamet";
-  // preference.genre = "Drama";
-  // preference.age = 42;
-  // preference.director = "Martin Scorsese";
-  // MovieRec mr;
-  // string genre = "Comedy";
-  // mr.movieRecommendationOverall(preference);
-  // // mr.movieRecByGenre(genre);
     userpass setup;
     prefs options;
     setup.startup(options);
